16178e08d50edd8db2d5b629ae25f9abb3639bc4
rwn <contact@0x57.me>
2024-09-11 15:16:34 +0200

    passes all tests

--- lab1/PE.vhdl
+++ lab1/PE.vhdl
@@ -27,12 +27,13 @@ end entity PE;
 
 architecture start of PE is
   constant SIGN                             : integer := wordlength-1;
+  constant MUL_SIGN                         : integer := 2*wordlength;
   signal signeda, signedb                   : signed(wordlength downto 0);                      -- signed inputs
   signal signedoutput                       : signed(wordlength-1 downto 0);                    -- signed output
+  signal mul_tmp                            : signed(2*wordlength+1 downto 0);                  -- temporary mul result
   signal before_shift, before_saturation    : signed(wordlength downto 0) := (others => '0');   -- Result before shift / saturation
   signal unsigned_shift                     : unsigned(shift_wordlength-1 downto 0);            -- unsigned shift_val
   signal V                                  : std_logic;                                        -- Overflow flag
-  signal mulus_tempus                       : signed(2*wordlength+1 downto 0);
 begin
   unsigned_shift <= unsigned(shift_val);
 
@@ -40,65 +41,32 @@ begin
   signeda <= resize(signed(inputa), signeda'length);
   signedb <= resize(signed(inputb), signedb'length);
 
-  operations: process (signeda, signedb, op, before_shift) is
-    variable mul_tmp : signed(2*wordlength+1 downto 0);
-    variable sign_check : signed(2*wordlength+1 downto wordlength);
+  operations: process (signeda, signedb, op, before_shift, mul_tmp) is
   begin
-    V <= '0';
-
     case op is
       when noop => null;
       when add =>
         before_shift <= signeda + signedb;
-
-        if ((signeda(SIGN) = signedb(SIGN)) and
-            (signeda(SIGN) /= before_shift(SIGN))) then
-            V <= '1';
-        end if;
       when sub =>
         before_shift <= signeda - signedb;
-
-        if ((signeda(SIGN)='0' and signedb(SIGN)='1' and before_shift(SIGN)='1') or
-            (signeda(SIGN)='1' and signedb(SIGN)='0' and before_shift(SIGN)='0')) then
-            V <= '1';
-        end if;
       when mul3over4a =>
-        mul_tmp := 3*signeda;
-        before_shift <= resize(shift_right(mul_tmp, 2), before_shift'length);
-      when mul7over8a =>
-        mul_tmp := 7*signeda;
-        before_shift <= resize(shift_right(mul_tmp, 3), before_shift'length);
-      when mul =>
-        mul_tmp := signeda * signedb;
+        mul_tmp <= shift_right(3*signeda, 2);
         before_shift <= resize(mul_tmp, before_shift'length);
-
-        if (mul_tmp(SIGN) = '0') then
-            sign_check := (others => '0');
-            if (mul_tmp(2*wordlength downto wordlength) /= sign_check) then
-                V <= '1';
-            end if;
-        else
-            sign_check := (others => '1');
-            if (mul_tmp(2*wordlength downto wordlength) /= sign_check) then
-                V <= '1';
-            end if;
-        end if;
+      when mul7over8a =>
+        mul_tmp <= shift_right(7*signeda, 3);
+        before_shift <= resize(mul_tmp, before_shift'length);
+      when mul =>
+        mul_tmp <= signeda*signedb;
+        before_shift <= resize(mul_tmp, before_shift'length);
       when shifta =>
         before_shift <= signeda;
       when nega =>
         before_shift <= -signeda;
-        if (signeda = -2048) then
-            V <= '1';
-        end if;
       when absa =>
         if signeda >= 0 then
             before_shift <= signeda;
         else
             before_shift <= -signeda;
-        end if;
-
-        if (signeda = -2048) then
-            V <= '1';
         end if;
       when others => null;
     end case;
@@ -109,17 +77,65 @@ begin
     before_saturation <= shift_right(before_shift, to_integer(unsigned_shift));
   end process shifting;
 
-  saturation: process (before_saturation, sat) is
+  overflow_check: process (signeda, signedb, mul_tmp, before_saturation) is
+    variable sign_check : signed(2*wordlength+1 downto wordlength);
+  begin
+    V <= '0';
+
+    case op is
+      when add =>
+        if ((signeda(SIGN) = signedb(SIGN)) and
+            (signeda(SIGN) /= before_saturation(SIGN))) then
+            V <= '1';
+        end if;
+      when sub =>
+        if ((signeda(SIGN)='0' and signedb(SIGN)='1' and before_saturation(SIGN)='1') or
+            (signeda(SIGN)='1' and signedb(SIGN)='0' and before_saturation(SIGN)='0')) then
+            V <= '1';
+        end if;
+      when mul =>
+        if mul_tmp(SIGN) = '0' then
+            sign_check := (others => '0');
+            if mul_tmp(2*wordlength downto wordlength) /= sign_check then
+                V <= '1';
+            end if;
+        else
+            sign_check := (others => '1');
+            if mul_tmp(2*wordlength downto wordlength) /= sign_check then
+                V <= '1';
+            end if;
+        end if;
+      when nega =>
+        if signeda = -2048 then
+            V <= '1';
+        end if;
+      when absa =>
+        if (before_saturation = -2048 or before_saturation = 2048) then
+            V <= '1';
+        end if;
+      when others => null;
+    end case;
+  end process overflow_check;
+
+  saturation: process (before_saturation, mul_tmp, sat, V) is
     constant MAX_VALUE : signed(wordlength downto 0) := to_signed(2047, before_saturation'length);
     constant MIN_VALUE : signed(wordlength downto 0) := to_signed(-2048, before_saturation'length);
   begin
       signedoutput <= before_saturation(wordlength-1 downto 0);
 
-      if sat = '1' then
-        if before_saturation > signed(MAX_VALUE) then
-          signedoutput <= resize(MAX_VALUE, signedoutput'length);
-        elsif before_saturation < signed(MIN_VALUE) then
-          signedoutput <= resize(MIN_VALUE, signedoutput'length);
+      if sat = '1' and V = '1' then
+        if op /= mul then
+            if before_saturation > MAX_VALUE then
+              signedoutput <= resize(MAX_VALUE, signedoutput'length);
+            else
+              signedoutput <= resize(MIN_VALUE, signedoutput'length);
+            end if;
+        else
+            if mul_tmp > MAX_VALUE then
+              signedoutput <= resize(MAX_VALUE, signedoutput'length);
+            else
+              signedoutput <= resize(MIN_VALUE, signedoutput'length);
+            end if;
         end if;
       end if;
   end process saturation;
