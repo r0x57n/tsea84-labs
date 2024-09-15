82a0bf71556c0b428b05de6575ff81c0fa3f0b95
rwn <contact@0x57.me>
2024-09-15 16:33:06 +0200

    lab2: op_type sequential

--- lab2/pe_types.vhdl
+++ lab2/pe_types.vhdl
@@ -3,8 +3,11 @@ use ieee.numeric_std.all;
 use ieee.math_real.all;
 use ieee.std_logic_1164.all;
 
+
 package pe_types is
   type op_type is (noop, add, sub, mul3over4a, mul7over8a, mul, shifta, nega, absa);  -- Operations
+  attribute enum_encoding : string;
+  attribute enum_encoding of op_type : type is "sequential";
 
   constant OP_LENGTH : integer := integer(ceil(log2(real(op_type'pos(op_type'right)+1))));  -- Length of op_type
 
